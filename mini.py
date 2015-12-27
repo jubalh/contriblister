c041344c315aac65c47304265c29428da214c99a
Michael Vetter <jubalh@openmailbox.org>
2015-12-27 01:28:28 +0100

    Use os.path.join

--- mini.py
+++ mini.py
@@ -12,7 +12,7 @@ from jinja2 import Environment, FileSystemLoader, Template
 def get_newest_repos():
     for repo in data["repos"]:
         sh.cd(repos_path)
-        current_repo_path = repos_path + "/" + repo["name"]
+        current_repo_path = os.path.join(repos_path, repo["name"])
 
         if os.path.isdir(current_repo_path):
             print("Updating " + repo["name"])
@@ -51,12 +51,12 @@ def count_overall_contributions():
             total_contributions += contributions
     return total_contributions
 
-
 with open("repos.json") as data_file:
     data = json.load(data_file)
 
+
 working_directory = os.getcwd()
-repos_path = working_directory + '/repos'
+repos_path = os.path.join(working_directory, 'repos')
 if not os.path.isdir(repos_path):
     os.mkdir(repos_path)
 
@@ -66,7 +66,7 @@ contributions_count = count_overall_contributions()
 
 #jinja_env = Environment(loader = FileSystemLoader(working_directory + '/templates'))
 #template = jinja_env.get_template('t.html')
-template = Template(open(working_directory+'/templates/t.html').read())
+template = Template(open(os.path.join(working_directory, 'templates', 't.html')).read())
 output = template.render(a_var=contributions_count)
 print(output)
 
